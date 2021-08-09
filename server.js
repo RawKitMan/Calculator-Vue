a03bd29036ebb399c2bb232789f769f61758c98c
CJ Vaughn <cj.vaughn052284@gmail.com>
2021-08-09 13:53:47 -0700

    server.js added

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+
+app = express();
+
+app.use(serveStatic(path.join(__dirname, 'dist')));
+
+app.list(process.env.PORT || 80);
