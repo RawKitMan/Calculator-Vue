09552e0767b7665bbee8feacadf0eba863b39920
CJ Vaughn <cj.vaughn052284@gmail.com>
2021-08-09 14:05:02 -0700

    Please

--- calculator-vue/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const serveStatic = require('serve-static');
-const path = require('path');
-
-app = express();
-
-app.use(serveStatic(path.join(__dirname, 'dist')));
-
-app.list(process.env.PORT || 80);
